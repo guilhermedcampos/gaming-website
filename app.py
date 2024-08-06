bc388fe5b7d18b0cce14da09ee7f677d08edfa2f
guilhermedcampos <guilhermedcampos7@gmail.com>
2024-08-06 01:11:42 +0100

    home page 1/x

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
+
+@app.route('/slot-machine')
+def slot_machine():
+    return render_template('slot-machine.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
