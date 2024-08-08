dc0b13d9465b2108efc29ce199754495ad7396d6
guilhermedcampos <guilhermedcampos7@gmail.com>
2024-08-09 00:39:32 +0100

    MPA 1/x

--- proj/app.js
+++ proj/app.js
@@ -16,6 +16,11 @@ app.get('/slot-machine', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'slot-machine.html'));
 });
 
+// Route for the profile page
+app.get('/profile', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'profile.html'));
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
