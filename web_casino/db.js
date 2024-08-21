e26b414847825715e3b6825bd02622661a398c45
guilhermedcampos <guilhermedcampos7@gmail.com>
2024-08-21 18:53:13 +0100

    changed db.js to include both options of lauching, either rds connection or simply localhost

--- web_casino/db.js
+++ web_casino/db.js
@@ -1,12 +1,12 @@
 const { Client } = require('pg');
 
-// Create a new instance of the Client
+// Check if environment variables are set, otherwise default to local configuration
 const client = new Client({
-  user: 'casino',     // Replace with your PostgreSQL user
-  host: 'localhost',        // Replace with your PostgreSQL host if different
-  database: 'casinogame',   // Replace with your database name
-  password: 'casino', // Replace with your PostgreSQL password
-  port: 5432,               // Default PostgreSQL port
+  user: process.env.DB_USER || 'casino',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'casinogame',
+  password: process.env.DB_PASSWORD || 'casino',
+  port: process.env.DB_PORT || 5432,
 });
 
 // Connect to the PostgreSQL database
@@ -16,3 +16,17 @@ client.connect()
 
 // Export the client for use in other modules
 module.exports = client;
+
+
+/*
+2. Set Environment Variables for RDS
+
+When you run your application in the environment where the RDS settings are used, you'll need to set the environment variables. You can set them directly in the terminal or through a configuration file.
+To set the environment variables:
+
+export DB_USER='your_rds_username'
+export DB_HOST='your_rds_host'
+export DB_NAME='your_rds_dbname'
+export DB_PASSWORD='your_rds_password'
+export DB_PORT=5432  
+*/
